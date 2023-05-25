4b4e060bcda2c3b84f5266759b7223df969e8967
Sujeong Artlab <sujeong@artlab.ai>
2023-05-25 22:59:44 +0900

    Initial Commit

---
+++ CaptureSample/CaptureSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureSampleApp.swift
+//  CaptureSample
+//
+//  Created by sujung Hwang on 2023/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CaptureSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
